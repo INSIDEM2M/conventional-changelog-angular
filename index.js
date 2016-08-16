d274ba558752c588b748fd7a184d3da1e1038465
Frederik Schubert <frederik.schubert@inside-m2m.de>
2016-08-16 13:42:20 +0200

    fix(transform): don't discard any commits

--- index.js
+++ index.js
@@ -42,34 +42,22 @@ function issueUrl() {
 
 var writerOpts = {
   transform: function(commit) {
-    var discard = true;
     var issues = [];
 
     commit.notes.forEach(function(note) {
       note.title = 'BREAKING CHANGES';
-      discard = false;
     });
 
     if (commit.type === 'feat') {
       commit.type = 'Features';
     } else if (commit.type === 'fix') {
       commit.type = 'Bug Fixes';
-    } else if (commit.type === 'perf') {
-      commit.type = 'Performance Improvements';
     } else if (commit.type === 'revert') {
       commit.type = 'Reverts';
-    } else if (discard) {
-      return;
-    } else if (commit.type === 'docs') {
-      commit.type = 'Documentation';
-    } else if (commit.type === 'style') {
-      commit.type = 'Styles';
-    } else if (commit.type === 'refactor') {
-      commit.type = 'Code Refactoring';
     } else if (commit.type === 'test') {
       commit.type = 'Tests';
     } else if (commit.type === 'chore') {
-      commit.type = 'Chores';
+      commit.type = 'Support/Maintenance/Internal';
     }
 
     if (commit.scope === '*') {
